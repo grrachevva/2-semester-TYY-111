657e27b54258b06f50fae91936b26ff5f3752e66
grrachevva <144918879+grrachevva@users.noreply.github.com>
2024-06-24 19:17:25 +0300

    Add files via upload

---
+++ Gracheva_14/Zadanie_14/VS_Project1/A_N_1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_N_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+            //нажатие клавиши мыши на кнопке Button1
+        {
+            //проверяем, была ли нажата правая кнопка мыши
+            if (e.Button == MouseButtons.Right)
+            {
+                //была нажата правая кнопка мыши
+                button2.Enabled = true; //делаем доступной вторую кнопку
+                button1.Enabled = false; //делаем недоступной первую кнопку
+            }
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        //нажатие клавиши мыши на кнопке Button2
+        {
+            //проверяем, была ли нажата правая кнопка мыши
+            if (e.Button == MouseButtons.Right)
+            {
+                //была нажата правая кнопка мыши
+                button1.Enabled = true; //делаем доступной первую кнопку
+                button2.Enabled = false; //делаем недоступной вторую кнопку
+            }
+        }
+    }
+}
