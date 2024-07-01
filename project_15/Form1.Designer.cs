651c66c00c89a6f399c11bc417abcf50c4004713
grrachevva <144918879+grrachevva@users.noreply.github.com>
2024-07-01 13:59:29 +0300

    Add files via upload

---
+++ project_15/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace A_N_2
+{
+    partial class Calendar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(378, 56);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.Enabled = false;
+            this.monthCalendar2.Location = new System.Drawing.Point(54, 56);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Календарь с промаркированными выходными и праздничными днями";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Заблокированный календарь";
+            // 
+            // Calendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 303);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar2);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Calendar";
+            this.Text = "Календарь";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ project_15/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace A_N_2
+{
+    public partial class Calendar : Form
+    {
+
+        private const string HolidayFileName = "holidays.txt";
+
+        public Calendar()
+        {
+            InitializeComponent();
+
+            //первый день недели - понедельник
+            monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
+
+            LoadHolidaysFromFile(HolidayFileName);
+            //отмечаем выходные дни полужирным шрифтом
+            SetWeekendsAsBoldedDates();
+        }
+
+
+        private void LoadHolidaysFromFile(string fileName)
+        {
+            monthCalendar1.RemoveAllBoldedDates();
+            DateTime holiday;
+            //отмечаем выходные дни
+            SetWeekendsAsBoldedDates();
+
+            using (StreamReader reader = new StreamReader(fileName, Encoding.GetEncoding(1251)))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (DateTime.TryParse(line, out holiday))
+                    {
+                        // Добавляем праздничный день к выделенным дат
+                        monthCalendar1.AddBoldedDate(holiday);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Неверный формат даты: {line}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            monthCalendar1.UpdateBoldedDates();
+        }
+
+        // Метод для выделения выходных дней (субботы и воскресенья)
+        private void SetWeekendsAsBoldedDates()
+        {
+            DateTime startDate = new DateTime(monthCalendar1.SelectionStart.Year, monthCalendar1.SelectionStart.Month, 1);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1); // Конец текущего месяца
+
+            while (startDate <= endDate)
+            {
+                if (startDate.DayOfWeek == DayOfWeek.Saturday || startDate.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    monthCalendar1.AddBoldedDate(startDate);
+                }
+                startDate = startDate.AddDays(1);
+            }
+
+            monthCalendar1.UpdateBoldedDates();
+        }
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            //загружаем из файла и отмечаем праздничные дни
+            LoadHolidaysFromFile(HolidayFileName);
+            //метод для выделения выходных дней
+            SetWeekendsAsBoldedDates();
+        }
+
+    }
+}
